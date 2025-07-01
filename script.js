fc531676b18962fdd3f6e5fd2c1e4dd99cc56f1e
Luis Henrique <luis951henrique@hotmail.com>
2025-07-01 13:59:06 -0300

    Commit inicial.

---
+++ script.js
@@ -0,0 +1,73 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleButton = document.getElementById("menu-toggle");
+    const nav = document.getElementById("nav");
+    const arrow = document.getElementById("arrow");
+
+    if (toggleButton && nav && arrow) {
+        toggleButton.addEventListener("click", () => {
+            const isVisible = nav.classList.toggle("show");
+            arrow.textContent = isVisible ? "▲" : "▼";
+        });
+    }
+});
+
+
+let slideAtual = 0;
+const slides = document.querySelectorAll(".slide");
+const btnProximo = document.querySelector(".right");
+const btnAnterior = document.querySelector(".left");
+let intervaloCarrossel;
+
+function mostrarSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove("active");
+        if (i === index) {
+            slides[index].classList.add("active");
+        }
+    });
+}
+
+function nextSlide() {
+    slideAtual = (slideAtual + 1) % slides.length;
+    mostrarSlide(slideAtual);
+}
+
+function prevSlide() {
+    slideAtual = (slideAtual - 1 + slides.length) % slides.length;
+    mostrarSlide(slideAtual);
+}
+
+function reset() {
+    clearInterval(intervaloCarrossel);
+    intervaloCarrossel = setInterval(proximoSlide, 5000);
+}
+
+btnProximo.addEventListener("click", () => {
+    nextSlide();
+    reset();
+});
+
+btnAnterior.addEventListener("click", () => {
+    prevSlide();
+    reset();
+});
+
+intervaloCarrossel = setInterval(nextSlide, 5000);
+
+
+function mostrarLista(id) {
+    const listas = document.querySelectorAll('.lista');
+    listas.forEach(lista => {
+        lista.style.display = 'none';
+    });
+
+    const selecionada = document.getElementById(id);
+    if (selecionada) {
+        selecionada.style.display = 'block';
+    }
+
+    const intro = document.getElementById('intro');
+    if (intro) {
+        intro.style.display = 'none';
+    }
+}
